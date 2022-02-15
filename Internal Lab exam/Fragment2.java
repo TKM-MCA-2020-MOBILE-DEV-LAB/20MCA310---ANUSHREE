42a0d799102a84e660e70390b5eba4cb946a0155
ANUSHREE-2021 <75942336+ANUSHREE-2021@users.noreply.github.com>
2022-02-15 18:47:05 +0530

    Internal Lab Exam

---
+++ Internal Lab exam/Fragment2.java
@@ -0,0 +1,33 @@
+package com.example.admin.foodbookingsystem;
+
+import android.arch.lifecycle.ViewModelProviders;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragment2 extends Fragment {
+
+    private Fragment2ViewModel mViewModel;
+
+    public static Fragment2 newInstance() {
+        return new Fragment2();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment2_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(Fragment2ViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ Internal Lab exam/fragment1.java
@@ -0,0 +1,33 @@
+package com.example.admin.foodbookingsystem;
+
+import android.arch.lifecycle.ViewModelProviders;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class fragment1 extends Fragment {
+
+    private Fragment1ViewModel mViewModel;
+
+    public static fragment1 newInstance() {
+        return new fragment1();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment1_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(Fragment1ViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
